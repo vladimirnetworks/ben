d3a0af8bf9e35ec06e58b2ec392c6234c751de0d
vladimirnetworks <vladimirnetworks@gmail.com>
2022-08-13 21:43:53 +0430

    update

--- resources/views/includes/topmenu.blade.php
+++ resources/views/includes/topmenu.blade.php
@@ -2,22 +2,43 @@
 
     <div class="row topmn ">
 
-      <div class="col-xs-1 text-center ham mr-4 d-lg-none">
+      <div class="col-1 text-center ham  d-lg-none">
+        <span class="mr-4">
         <div></div>
         <div></div>
         <div></div>
+        </span>
+
       </div>
 
 
-      <div class="col text-center text-lg-right" style="font-weight:bold;font-size:max(2vw, 1rem);"> مجله‌ی اینترنتی
-        بهنام</div>
+      <div id="mbtitle" class="col text-center text-lg-right" style="font-weight:bold;font-size:max(2vw, 1rem);">
+        <a href="/" style="color:white"> مجله‌ی اینترنتی
+          بهنام</a>
+
+      </div>
 
 
-
-      <div class="col-xs-1 text-center ham ml-4">
+      <div id="mbmag" class="col-1 text-center ham ml-4">
         <img src="https://www.behkiana.ir/icons/mag.png?" style="width:3rem">
-
       </div>
+
+
+      <div id="mbsrch" class="col-11 " style="display:none">
+        <div class="bg-white rounded">
+          <img src="https://www.behkiana.ir/icons/mag.png?" style="width:3rem">
+        </div>
+      </div>
+
+      <script>
+        $("#mbmag").fadeOut();
+        $("#mbtitle").fadeOut(function() {
+          $("#mbsrch").fadeIn()
+        });
+
+
+      </script>
+
 
 
     </div>
@@ -29,4 +50,5 @@
     @endforeach
 
   --}}
+  
   </div>
