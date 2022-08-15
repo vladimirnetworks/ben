db83bb96963c2a58615f9ba32a68c6c4bb6a0952
vladimirnetworks <vladimirnetworks@gmail.com>
2022-08-15 19:15:12 +0430

    update using fastpush

--- resources/views/blogs/post.blade.php
+++ resources/views/blogs/post.blade.php
@@ -2,25 +2,33 @@
 
 @section('main')
 
-
-<div class="m-3 p-3 bg-white rounded">
-    <h1 class="h2">
-        {{$post->title}}
-    </h1>
-
-    <div class="m-2 bpost"> {!! $post->text !!} </div>
-
+<div class="row" style="margin:0px">
+<div class="col-12 col-md-9">
+    <div class="my-3 p-3 bg-white rounded">
+        <h1 class="h2">
+            {{$post->title}}
+        </h1>
+    
+        <div class="m-2 bpost"> {!! $post->text !!} </div>
+    
+    </div>
 </div>
 
-<div class="row m-0">
-
-    <div style="" class="col-md-6">
-        <div class="col-md-12 bg-white rounded">
+<div class="col-12 col-md-3">
+  
+        <div class=" bg-white rounded my-3 py-3">
         @foreach($related as $post)
         @include("blogs.includes.list")
         @endforeach
         </div>
-    </div>
+    
+</div>
+</div>
+
+
+<div class="row m-0">
+
+  
 
    <!-- <div style="" class="col-md-6 ">
         <div class="col-md-12 bg-white rounded">
