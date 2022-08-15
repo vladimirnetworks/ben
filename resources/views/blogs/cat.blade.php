fef233ac40b9bb7b73cd3d8cdd2280210df2aee3
vladimirnetworks <vladimirnetworks@gmail.com>
2022-08-15 11:14:23 +0430

    update using fastpush

--- resources/views/blogs/cat.blade.php
+++ resources/views/blogs/cat.blade.php
@@ -6,14 +6,16 @@
 
 <div  class="m-3 bg-white rounded row">
     @foreach($posts as $post)
+    <div class="col-12 col-md-4">
       @include("blogs.includes.list") 
+    </div>
     @endforeach
 </div>
 
 
 
 @if($pageinate->currentPage() > 2)
-<a href="/cat/{{$cat}}/index/{{$pageinate->currentPage()-1}}">prev</a>
+<a href="/cat/{{$cat}}/{{$pageinate->currentPage()-1}}">prev</a>
 @endif
 
 @if($pageinate->currentPage() == 2)
@@ -21,7 +23,7 @@
 @endif
 
 @if($pageinate->currentPage() < $pageinate->lastPage())
-    <a href="/cat/{{$cat}}/index/{{$pageinate->currentPage()+1}}">next</a>
+    <a href="/cat/{{$cat}}/{{$pageinate->currentPage()+1}}">next</a>
 @endif
 
 
