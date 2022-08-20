479cf74f53e364b5016fd9a2285ed5ad7626dc0f
vladimirnetworks <vladimirnetworks@gmail.com>
2022-08-20 20:33:07 +0430

    update using fastpush

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,8 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: *');
-header('Access-Control-Allow-Headers: *');
-header('bapptype: 135');
+#header('Access-Control-Allow-Origin: *');
+#header('Access-Control-Allow-Methods: *');
+#header('Access-Control-Allow-Headers: *');
+#header('bapptype: 135');
 /*
 |--------------------------------------------------------------------------
 | Create The Application
