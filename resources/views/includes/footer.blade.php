12a3b3d4667310877d6af2abe43ed1f57ba427e6
vladimirnetworks <vladimirnetworks@gmail.com>
2022-09-01 14:47:15 +0430

    update using fastpush

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,16 +1,6 @@
 <div class="p-4 p-md-5 mt-4 text-white bg-dark">
 
-  <script type="text/javascript">
-    (function(){
-    var now = new Date();
-    var head = document.getElementsByTagName('head')[0];
-    var script = document.createElement('script');
-    script.async = true;
-    var script_address = 'https://cdn.yektanet.com/js/benham.ir/native-benham.ir-27361.js';
-    script.src = script_address + '?v=' + now.getFullYear().toString() + '0' + now.getMonth() + '0' + now.getDate() + '0' + now.getHours();
-    head.appendChild(script);
-    })();
-</script>
+
 
 
 
