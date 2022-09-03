48bcd666397ada975b9978d50ce8f9f4eb07c9eb
vladimirnetworks <vladimirnetworks@gmail.com>
2022-09-03 16:20:57 +0430

    update using fastpush

--- resources/views/includes/headers.blade.php
+++ resources/views/includes/headers.blade.php
@@ -42,7 +42,18 @@
   var sabaVisionWebsitePage = "ALL"
 </script>
 
-<script src="https://plus.sabavision.com/dox/dox.min.js"></script>
+
+<script>
+  (function(){
+  var now = new Date();
+  var head = document.getElementsByTagName('head')[0];
+  var script = document.createElement('script');
+  script.async = true;
+  var script_address = 'https://plus.sabavision.com/dox/dox.min.js';
+  script.src = script_address + '?v=' + now.getFullYear().toString() + '0' + now.getMonth() + '0' + now.getDate() + '0' + now.getHours();
+  head.appendChild(script);
+  })();
+</script>  
 
 
 <script>
