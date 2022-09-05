19db627776dc17b8f44ffe8ef2d498c970eb0a0f
vladimirnetworks <vladimirnetworks@gmail.com>
2022-09-05 18:29:16 +0430

    update using fastpush

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -22,13 +22,13 @@ class PostController extends start
     public function sitemap()
     {
 
-      //  $latest =  $this->domain()->posts->orderBy('id', 'DESC')->take(100)->get();
+        //  $latest =  $this->domain()->posts->orderBy('id', 'DESC')->take(100)->get();
 
 
         $latest = $this->domain()->posts->orderBy('updated_at', 'desc')->limit(1000)->get();
 
 
-     
+
 
         // dd($posts[0]->domain->domain);
 
@@ -42,7 +42,7 @@ class PostController extends start
 
         foreach ($latest as $hitx) {
 
-            $linkpage = "https://" . $hitx->domain->domain . "/post/" . $hitx->url ."-".$hitx->id. '.html';
+            $linkpage = "https://" . $hitx->domain->domain . "/post/" . $hitx->url . "-" . $hitx->id . '.html';
             $lastmodx = '<lastmod>' . $hitx->updated_at . '+03:30</lastmod>';
 
 
@@ -162,9 +162,83 @@ class PostController extends start
      * @param  \App\Models\post  $post
      * @return \Illuminate\Http\Response
      */
+
+
+    public static function xben($mm, $k)
+    {
+
+        $aa = '
+        <div id="b' . $k . 'video-player"></div>
+        <script>
+        jwplayer("b' . $k . 'video-player").setup({
+    
+        "playbackRateControls": "true",
+        "timeSliderAbove": "true",
+        "width": "100%",
+        "aspectratio": "16:9",
+        "skin":
+        {
+        "active": "#E20022",
+       
+        "controlbar": {
+        "iconsActive": "#f00",
+        "icons": "#fff"
+        },
+        "timeslider": {
+        "progress": "#f00"
+        },
+        "menus": {
+        "textActive": "#f00"
+        }
+        },
+            
+                "qualityLabels": { },
+            
+        "playlist": [{
+        "image": "",
+        "sources": [
+    
+            
+                {
+                "file": "' . $mm . '",
+                "type": "dash"
+                }
+            
+        ],
+    
+        }],
+    
+            
+                "dash": "shaka",
+            
+    
+    
+        "autostart": "viewable",
+    
+        "logo": {
+            "file": "",
+            "position": "bottom-right"
+        },
+    
+        "plugins": {
+        }
+        });
+    
+    
+    
+    
+    </script>';
+        return $aa;
+    }
+
+
     public function show($url, $id)
     {
 
+        $benmash = '<script src="https://static.namasha.com/content/player8.11.10/jwplayer.js"></script>
+        <script>jwplayer.base = "https://static.namasha.com/content/player8.11.10/";</script>
+        <script>jwplayer.key = "kukmSGInuqM0shRLwJjVLWeWAxLakiKS90Fg2+vhQks=";</script>
+        ';
 
 
         $posts = Post::whereDomainId($this->domain()->id)->orderBy('id', 'asc')->paginate(10, ['*'], 'page', 0);
@@ -172,6 +246,16 @@ class PostController extends start
         $post = $this->domain()->posts->whereId($id)->get()[0];
 
 
+        if (preg_match("!https://.*?.namasha.com/dash/.*?/Manifest.mpd!", $post->text)) {
+            $post->text  = $benmash . $post->text;
+
+            preg_match_all("!(https://.*?.namasha.com/dash/.*?/Manifest.mpd)!", $post->text, $m);
+            foreach ($m[1] as $k => $hh) {
+                $zz[] = $this::xben($hh, $k);
+            }
+
+            $post->text = str_replace($m[1], $zz, $post);
+        }
 
         $related = json_decode($this->domain()->related_posts);
 
