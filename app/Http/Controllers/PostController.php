7be089bbb8e6c026413b69aef45039215f0a505c
vladimirnetworks <vladimirnetworks@gmail.com>
2022-10-22 11:24:15 +0330

    update using fastpush

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -235,7 +235,7 @@ class PostController extends start
  
 
            setTimeout(function() {
-            jwplayer("b' . $k . 'video-player").setMute(false);
+           // jwplayer("b' . $k . 'video-player").setMute(false);
             
         },5000);      
 
